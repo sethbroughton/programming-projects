0e8629415366f9802e68912736cf0ec5ce5392e3
Seth Broughton <broughton.24@gmail.com>
2020-05-24 15:40:55 -0400

    API functioning

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,33 +1,19 @@
 //create a context menu
 chrome.contextMenus.create({
     //string to display on menu
-    'title': 'Search IMDB for "%s"',
+    'title': 'Search for current stock price',
     //contexts here is selection as we want to extract the highlighted text.
     'contexts': ['selection'],
     //the event handler
     'onclick': async (context) => {
         const name = context.selectionText;
-        const response = await fetch(`https://www.omdbapi.com/?t=${name}&apikey=e48e70b4`)
+        const response = await fetch(`https://sandbox.iexapis.com/stable/stock/${name}/quote?token=Tsk_ce46deb86c0d49e389b13e82b92f5808`)
         const {
-            Title,
-            Year,
-            Runtime,
-            Genre,
-            Actors,
-            imdbRating
+            latestPrice
+            
         } = await response.json()
         const newLine = "\r\n"
-        let message = `Title : ${Title}`
-        message += newLine
-        message += `Year : ${Year}`
-        message += newLine
-        message += `Runtime : ${Runtime}`
-        message += newLine
-        message += `Genre : ${Genre}`
-        message += newLine
-        message += `Actors : ${Actors}`
-        message += newLine
-        message += `IMDb Rating : ${imdbRating}`
+        let message = `Price : ${latestPrice}`
         alert(message)
     }
 });
--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,11 +1,2 @@
 //content.js
-const fetchRatings = () => {
-    $(".media-lists__td-title").map(async function () {
-        const name = this.innerText;
-        const response = await fetch(`https://www.omdbapi.com/?t=${name}&apikey=e48e70b4`)
-        const {imdbRating} = await response.json()
-        this.innerText = `${name} (${imdbRating})`
-    })
-}
-
-fetchRatings();
+alert("Did it work?")
